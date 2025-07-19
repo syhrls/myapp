f8211e4e4306a5127091748bf1500a8199b4d42c
Syahril Ramadhan <rodyulo20@gmail.com>
2025-07-20 03:54:31 +0700

    test env

--- main.go
+++ main.go
@@ -5,15 +5,27 @@ import (
 	"example/hello/routes"
 	"example/hello/utils"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(".env file not found or failed to load")
+	}
 
-	r := gin.New()                    // gunakan gin.New() agar tidak include logger default
-	r.Use(gin.Recovery())             // tangani panic
+	mode := os.Getenv("GIN_MODE")
+	if mode == "" {
+		mode = gin.DebugMode
+	}
+	gin.SetMode(mode)
+
+	r := gin.Default()    // gunakan gin.New() agar tidak include logger default
 
 	routes.SetupRoutes(r)
 
@@ -27,6 +39,7 @@ func main() {
 	if port == "" {
 		port = "8080"
 	}
+	
 	fmt.Println("Server running on port:", port)
 	r.Run(":" + port)
 }
