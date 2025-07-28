da862237b2c23c5014c53f1045ec7efdb12a2acb
Syahril Ramadhan <rodyulo20@gmail.com>
2025-07-28 16:34:41 +0700

    Add logging middleware to auth routes

--- routes/v1/auth_routes.go
+++ routes/v1/auth_routes.go
@@ -3,12 +3,13 @@ package v1
 import (
 	"example/hello/database"
 	"example/hello/handlers"
+	"example/hello/utils"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupAuthRoutes(r *gin.RouterGroup) {
-	auth := r.Group("/v1/auth")
+	auth := r.Group("/v1/auth", utils.LogStartEnd())
 	{
 		auth.POST("/register", handlers.RegisterHandler(database.DB))
 		auth.POST("/login", handlers.LoginHandler(database.DB))
